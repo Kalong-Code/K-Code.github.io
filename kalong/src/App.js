0d7990cdb9e2b8ef21072f521abf32926c820271
Kalong-Code <kalongboniface97@gmail.com>
2022-06-04 12:57:33 +0000

    nav bar added

--- kalong/src/App.js
+++ kalong/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Home  from './components/Home';
 import './App.css';
-
+// import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    < Home />
+    <>
+      
+         < Home /> 
+        
+      
+    </>
+    
   );
 }
 
